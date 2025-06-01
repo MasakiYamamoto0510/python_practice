afdfcee284e672742af579591a91296775a13b88
Masaki Yamamoto <yamamotosaga.miha@gmail.com>
2025-06-01 14:17:35 +0900

    [Practice] キーワード引数

--- practice.py
+++ practice.py
@@ -1,21 +1,13 @@
-#def say_something(word, word2, word3):
-#    print(word)
-#    print(word2)
-#    print(word3)
-#
-#say_something('Hello', 'Mike', 'Nancy')
-
-#def say_something(*args):
-#    print(args)
-#
-#say_something('Hello', 'Mike', 'Nancy')
+def menu(**kwargs):
+   # print(kwargs)
+  for k, v in kwargs.items():
+      print(k, v)
 
 
-def say_something(*args):
-    for arg in args :
-        print(arg)
+d = {
+    'entree': 'beef',
+    'drink': 'ice coffe',
+    'desert': 'ice'
+}
 
-say_something('Hello', 'Mike', 'Nancy')
-
-t = ('Mike', 'Nancy')
-say_something('Hi!', *t)
+menu(**d)
