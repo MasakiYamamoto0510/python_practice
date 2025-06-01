a6b40b9ce908ab05f06871b4a3d0a75e2662cf81
Masaki Yamamoto <yamamotosaga.miha@gmail.com>
2025-06-01 13:13:59 +0900

    [Practice] zip関数

--- practice.py
+++ practice.py
@@ -1,12 +1,6 @@
-# num_list = [0,1,2,3,4,5,6,7,8,9]
-# for i in num_list:
-#    print(i)
+days = ['Mon', 'Tue', 'Wed']
+fruits = ['apple', 'banana', 'orange']
+drinks = ['coffe', 'tea', 'beer']
 
-# for i in range(2, 10, 3):
-#    print(i)
-
-#for i in range(10):
-#    print(i, 'hello')
-
-for _ in range(10):
-    print('hello')
+for day, fruit, drink in zip(days, fruits, drinks):
+    print(day, fruit, drink)
