9ef83ca9c41cb8894c87b0eb27e337781a5218d7
Masaki Yamamoto <yamamotosaga.miha@gmail.com>
2025-06-01 14:05:31 +0900

    [Practice] 位置引数のタプル化

--- practice.py
+++ practice.py
@@ -1,9 +1,21 @@
-d = {'x': 100, 'y': 200}
+#def say_something(word, word2, word3):
+#    print(word)
+#    print(word2)
+#    print(word3)
+#
+#say_something('Hello', 'Mike', 'Nancy')
 
-for k, v in d.items():
-    print(k, ':', v)
+#def say_something(*args):
+#    print(args)
+#
+#say_something('Hello', 'Mike', 'Nancy')
 
-d2 = {'z': 1000, 'a': 2000}
 
-for k, v in d2.items():
-    print(k, ':', v)
+def say_something(*args):
+    for arg in args :
+        print(arg)
+
+say_something('Hello', 'Mike', 'Nancy')
+
+t = ('Mike', 'Nancy')
+say_something('Hi!', *t)
