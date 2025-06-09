c57e370b49e5bf3230755b4297f152ef15d3ef6e
Masaki Yamamoto <yamamotosaga.miha@gmail.com>
2025-06-09 13:59:00 +0900

    [Peactice] import文とas

---
+++ lesson.py
@@ -0,0 +1,11 @@
+# import lesson_package.utils
+# from lesson_package import utils
+from lesson_package.utils import say_twice
+
+# r = lesson_package.utils.say_twice('hello')
+
+# r = utils.say_twice('hello')
+
+r = say_twice('hello')
+
+print(r)
